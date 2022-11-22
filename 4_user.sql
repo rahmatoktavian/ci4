c791dbf0e1f352c2db8c67885df92feed59dc323
rahmat <cememeo@rahmat.local>
2022-11-22 08:51:45 +0700

    init

--- 4_user.sql
+++ 4_user.sql
@@ -1,3 +1,5 @@
+DROP TABLE `user`;
+
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
@@ -7,4 +9,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `username`, `password`, `tipe`) VALUES
 (1, 'petugas', '670489f94b6997a870b148f74744ee5676304925', 'staff'),
-(2, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'administrator');
+(2, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin');
+
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
